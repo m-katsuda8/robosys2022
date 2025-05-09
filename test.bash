4f0c8d4e035c40a4804fb9e0912a60953de33ef3
masayuki katsuda <m.katsuda8@gmail.com>
2025-05-09 18:06:32 +0900

    Add tests

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+:#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Masayuki Katsuda
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -13,7 +13,17 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng $LINENO
 
+###STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng $LINENO  
+[ "${out}" = "" ] || ng $LINENO
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
+
 [ "$res" = 0 ] && echo OK
+exit $RES
 exit $res
 
 
